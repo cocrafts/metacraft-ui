f397d7e6d289bfd4d3c0ff61f0934144f96f12f7
kayaba <kayaba.fluctlight@gmail.com>
2022-04-08 01:52:20 +0700

    expose modal (valtio state) library consumer

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ export * from './components/Button';
 export * from './components/Hoverable';
 export * from './components/ModalManager';
 export * from './utils/store/app';
+export * from './utils/store/modal';
 export * from './utils/store/theme';
