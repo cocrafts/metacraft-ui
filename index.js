339c108b61bdcf1de8cae49b41a46a75d51d24e9
kayaba <kayaba.fluctlight@gmail.com>
2022-04-07 18:06:37 +0700

    latest deps, ignore react-native-gesture-handler update warning

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 
 import App from './src/playground';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+LogBox.ignoreLogs([
+	"[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
