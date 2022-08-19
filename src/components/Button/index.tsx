45ca19a99832d88af54db2d750ced5ecec9adb89
kayaba <kayaba.fluctlight@gmail.com>
2022-08-20 00:23:56 +0700

    upgrade Button component, with smooth animation

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,42 +1,84 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import {
 	StyleProp,
 	StyleSheet,
-	Text,
 	TextStyle,
 	TouchableOpacity,
 	ViewStyle,
 } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, {
+	SharedValue,
+	useAnimatedStyle,
+	withSpring,
+} from 'react-native-reanimated';
+import { getLuminance } from 'color2k';
+import { ThemeState, themeState } from 'utils/state/theme';
+import { useSnapshot } from 'valtio';
 
 import Hoverable from '../Hoverable';
+import Text from '../Text';
 
-import { HoveredStyleFunc, useDefaultHoveredStyle } from './shared';
+import { getHoverColors } from './helper';
 
 const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
 
 interface Props {
 	style?: StyleProp<ViewStyle>;
-	useHoveredStyle?: HoveredStyleFunc;
+	outline?: boolean;
 	title?: string;
-	titleStyle?: TextStyle;
+	titleStyle?: StyleProp<TextStyle>;
 	onPress?: () => void;
+	children?: ReactNode;
+	prefix?: ReactNode;
+	suffix?: ReactNode;
 }
 
 export const Button: FC<Props> = ({
 	style,
-	useHoveredStyle = useDefaultHoveredStyle,
-	title = 'Button Title',
+	outline,
+	title,
 	titleStyle,
 	onPress,
+	children,
+	prefix,
+	suffix,
 }) => {
+	const { colors } = useSnapshot<ThemeState>(themeState);
+	const containerStyle = [
+		styles.container,
+		{ backgroundColor: colors.primary },
+		style,
+	];
+	const { backgroundColor, borderColor } = StyleSheet.flatten(containerStyle);
+	const lightBg = getLuminance(backgroundColor as string) > 0.5;
+	const hoverColors = getHoverColors(
+		backgroundColor as string,
+		borderColor as string,
+		outline,
+	);
+
+	const useHoveredStyle = (isHovered: SharedValue<boolean>) => {
+		return useAnimatedStyle(() => {
+			const [bg, hoveredBg] = hoverColors.background;
+			const [border, hoveredBorder] = hoverColors.border;
+
+			return {
+				backgroundColor: withSpring(isHovered.value ? hoveredBg : bg),
+				borderColor: withSpring(isHovered.value ? hoveredBorder : border),
+			};
+		}, [backgroundColor]);
+	};
+
 	return (
-		<Hoverable
-			style={[styles.container, style]}
-			useHoveredStyle={useHoveredStyle}
-		>
+		<Hoverable style={containerStyle} animatedStyle={useHoveredStyle}>
 			<AnimatedTouchable onPress={onPress}>
-				<Text style={[styles.title, titleStyle]}>{title}</Text>
+				{prefix}
+				{children || (
+					<Text style={[styles.title, lightBg && styles.darkTitle, titleStyle]}>
+						{title}
+					</Text>
+				)}
+				{suffix}
 			</AnimatedTouchable>
 		</Hoverable>
 	);
@@ -46,12 +88,22 @@ export default Button;
 
 const styles = StyleSheet.create({
 	container: {
-		backgroundColor: '#3397fc',
-		borderRadius: 5,
-		paddingVertical: 12,
-		paddingHorizontal: 18,
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderRadius: 100,
+		backgroundColor: 'transparent',
+		borderWidth: 2,
+		borderColor: 'rgba(255, 255, 225, 0.1)',
 	},
 	title: {
+		flex: 1,
 		color: '#FFFFFF',
+		fontWeight: '400',
+		textAlign: 'center',
+		paddingVertical: 6,
+		paddingHorizontal: 14,
+	},
+	darkTitle: {
+		color: '#222222',
 	},
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,32 +1,27 @@
 import React, { FC, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Button from 'components/Button';
+import Text from 'components/Text';
 import {
 	AnimateDirections,
 	BindDirections,
 	modalActions,
 } from 'utils/state/modal';
+import { themeState } from 'utils/state/theme';
+import { useSnapshot } from 'valtio';
 
 import Popup from './Popup';
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	buttonContainer: {
-		marginTop: 20,
-	},
-});
-
 export const HomeScreen: FC = () => {
 	const containerRef = useRef<View>(null);
+	const { colors } = useSnapshot(themeState);
+	const containerStyle = { backgroundColor: colors.background };
+
 	const showPopup = () => {
 		modalActions.show({
 			id: 'Cloud Le',
 			component: Popup,
-			bindingDirection: BindDirections.Top,
+			bindingDirection: BindDirections.Inner,
 			animateDirection: AnimateDirections.TopRight,
 			bindingRef: containerRef,
 			maskStyle: {
@@ -41,16 +36,32 @@ export const HomeScreen: FC = () => {
 	};
 
 	return (
-		<View style={styles.container}>
+		<View style={[styles.container, containerStyle]}>
 			<View ref={containerRef} style={{ width: 20 }}>
 				<Text>W</Text>
 			</View>
 			<Text>
 				Still too early to have something to show.. but this going to be fun!
 			</Text>
-			<Button style={styles.buttonContainer} onPress={showPopup} />
+			<Button
+				outline
+				style={styles.buttonContainer}
+				onPress={showPopup}
+				title="Show popup"
+			/>
 		</View>
 	);
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	buttonContainer: {
+		marginTop: 20,
+	},
+});
